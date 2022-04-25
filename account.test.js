dba2a16ba0fc5a5ad5aaa7ebf07ba39cf89c3a58
Joe Thornton <thornton.jp@gmail.com>
2022-04-25 16:00:58 +0100

    refactor tests to create new instance of account before each test

--- account.test.js
+++ account.test.js
@@ -1,32 +1,32 @@
 const Account = require("./account");
 
 describe("account", () => {
-  it("creates a new account with a balance of 0", () => {
+  beforeEach((done) => {
     account = new Account();
+    done();
+  });
+
+  it("creates a new account with a balance of 0", () => {
     expect(account.get_balance()).toEqual(0);
   });
 
   it("deposits money and displays the balance", () => {
-    account = new Account();
     account.deposit(10);
     expect(account.get_balance()).toEqual(10);
   });
 
   it("deposits a different amount and displays the balance", () => {
-    account = new Account();
     account.deposit(20);
     expect(account.get_balance()).toEqual(20);
   });
 
   it("can take 2 positive integer deposits", () => {
-    account = new Account();
     account.deposit(10);
     account.deposit(20);
     expect(account.get_balance()).toEqual(30);
   });
 
   it("deposit does not accept a negative amount", () => {
-    account = new Account();
     expect(() => {
       account.deposit(-1);
     }).toThrow("Invalid input: amounts must be must be greater than 0");
