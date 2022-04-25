647a4f59c9423d2db5962bf522a9d84a752960f0
Joe Thornton <thornton.jp@gmail.com>
2022-04-25 18:50:10 +0100

    refactored to create a transaction class

--- account.js
+++ account.js
@@ -1,22 +1,24 @@
 class Account {
   constructor() {
-    this.balance = 0;
+    this.transactions = [];
   }
 
   deposit(amount) {
-    this.checkValidAmount(amount);
-    this.balance += amount;
+    this.checkAmountIsValidNumber(amount);
+    this.transactions.push(new Transaction(amount));
   }
 
   withdraw(amount) {
-    this.balance -= amount;
+    this.transactions.push(new Transaction(-amount));
   }
 
   getBalance() {
-    return this.balance;
+    return this.transactions
+      .map((t) => t.getAmount())
+      .reduce((acc, prev) => acc + prev, 0);
   }
 
-  checkValidAmount(amount) {
+  checkAmountIsValidNumber(amount) {
     const decimalPattern = /^\d+(\.\d{1,2})?$/;
     if (amount <= 0) {
       throw "Invalid input: amounts must be must be greater than 0";
@@ -25,6 +27,16 @@ class Account {
     } else if (!decimalPattern.test(amount)) {
       throw "Invalid input: amount cannot have amounts that are fractions of a pence";
     }
+  }
+}
+
+class Transaction {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  getAmount() {
+    return this.amount;
   }
 }
 
