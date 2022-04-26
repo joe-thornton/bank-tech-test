c3e28847402ee33d7c9f78720748c293252cfd18
Joe Thornton <thornton.jp@gmail.com>
2022-04-26 18:56:52 +0100

    statement working for debits

--- statement.js
+++ statement.js
@@ -14,9 +14,13 @@ class Statement {
       this.transactions.forEach((t) => {
         currentTotal = currentTotal + t.getAmount();
         const dateString = this.getDateText(t.getDate());
-        const transactionText = `${dateString} || ${t
-          .getAmount()
-          .toFixed(2)} || || ${currentTotal.toFixed(2)}`;
+        const creditOrDebitString =
+          t.getAmount() > 0
+            ? ` || ${t.getAmount().toFixed(2)} || || `
+            : ` || || ${Math.abs(t.getAmount()).toFixed(2)} || `;
+        const transactionText = `${dateString}${creditOrDebitString}${currentTotal.toFixed(
+          2
+        )}`;
         transactionTexts.push(transactionText);
       });
       const transactionsTextReversed = transactionTexts.reverse().join("\n");
--- statement.test.js
+++ statement.test.js
@@ -41,27 +41,27 @@ describe("statement", () => {
     );
   });
 
-  // it("statement returns 3 transactions where one is a", () => {
-  //   const getDate = jest
-  //     .fn()
-  //     .mockImplementation(() => new Date(2021, 0, 10, 0, 0, 0));
-  //   let transactions = [
-  //     {
-  //       getAmount: jest.fn().mockReturnValue(1000),
-  //       getDate: jest.fn().mockReturnValue(new Date(2021, 0, 10, 0, 0, 0)),
-  //     },
-  //     {
-  //       getAmount: jest.fn().mockReturnValue(2000),
-  //       getDate: jest.fn().mockReturnValue(new Date(2021, 0, 13, 0, 0, 0)),
-  //     },
-  //     {
-  //       getAmount: jest.fn().mockReturnValue(-500),
-  //       getDate: jest.fn().mockReturnValue(new Date(2021, 0, 14, 0, 0, 0)),
-  //     },
-  //   ];
-  //   statement = new Statement(transactions);
-  //   expect(statement.getText()).toEqual(
-  //     "date || credit || debit || balance\n14/01/2021 || || 500.00 || 2500.00\n13/01/2021 || 2000.00 || || 3000.00\n10/01/2021 || 1000.00 || || 1000.00"
-  //   );
-  // });
+  it("statement returns 3 transactions where one is a", () => {
+    const getDate = jest
+      .fn()
+      .mockImplementation(() => new Date(2021, 0, 10, 0, 0, 0));
+    let transactions = [
+      {
+        getAmount: jest.fn().mockReturnValue(1000),
+        getDate: jest.fn().mockReturnValue(new Date(2021, 0, 10, 0, 0, 0)),
+      },
+      {
+        getAmount: jest.fn().mockReturnValue(2000),
+        getDate: jest.fn().mockReturnValue(new Date(2021, 0, 13, 0, 0, 0)),
+      },
+      {
+        getAmount: jest.fn().mockReturnValue(-500),
+        getDate: jest.fn().mockReturnValue(new Date(2021, 0, 14, 0, 0, 0)),
+      },
+    ];
+    statement = new Statement(transactions);
+    expect(statement.getText()).toEqual(
+      "date || credit || debit || balance\n14/01/2021 || || 500.00 || 2500.00\n13/01/2021 || 2000.00 || || 3000.00\n10/01/2021 || 1000.00 || || 1000.00"
+    );
+  });
 });
